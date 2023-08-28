4767b1c97afce9f1943677bab321f56376f3d93a
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-08-28 18:41:20 +0200

    Nearly there I just need to remove the project_id.

--- 03-ruby-medium-article-slurper/test.rb
+++ 03-ruby-medium-article-slurper/test.rb
@@ -3,47 +3,91 @@
 require 'net/http'
 require 'nokogiri'
 require 'action_view'
+require 'googleauth'
+
+require_relative 'lib_genai'
+
+Prompt = <<-END_OF_PROMPT
+Provide a summary for the following articles.
+
+* Please write about the topics, the style, and rate the article from 1 to 10 in terms of accuracy or professionalism.
+* Please also tell me, for each article, whether it talks about Google Cloud.
+* Can you guess the nationality of the person writing the articles?
+
+--
+
+END_OF_PROMPT
 
 def init()
     Dir.mkdir('inputs/') rescue nil
     Dir.mkdir('outputs/') rescue nil
 end
 
-def fetch_from_medium(medium_user)
+def fetch_from_medium(medium_user, _opts={})
+    opts_refetch_if_exists = _opts.fetch :refetch_if_exists, false
+    xml_filename = "inputs/medium-feed.#{medium_user}.xml"
+    genai_input_filename = "inputs/medium-latest-articles.#{medium_user}.txt"
+
+    if File.exist?(genai_input_filename) and (not opts_refetch_if_exists )
+        puts "File '#{genai_input_filename}' already exists.. wont reparse"
+        return nil 
+    end 
+
+    # Downloading the file and iterating through article parts.
     medium_url = "https://medium.com/feed/@#{medium_user}"
     xml_response = Net::HTTP.get(URI(medium_url))
-    File.open("inputs/medium-feed.#{medium_user}.xml", 'w') do |f|
+    File.open(xml_filename, 'w') do |f|
         f.write xml_response
     end
     # deprecated 
     #publications = "https://api.medium.com/v1/users/#{medium_user}/publications?format=json"
     docSM = Nokogiri::XML(xml_response)
+
+    
     # Looks like my articles are under many <content:encoded> tags, so here you go..
-    docSM.xpath("//content:encoded").each_with_index do |node,ix|
-        puts "* Article #{ix+1}:"
-        puts ActionView::Base.full_sanitizer.sanitize(node.inner_text)
-        puts ''
+    File.open(genai_input_filename, 'w') do |file| # file.write("your text") }
+        docSM.xpath("//content:encoded").each_with_index do |node,ix|
+            # puts "* Article #{ix+1}:"
+            # puts ActionView::Base.full_sanitizer.sanitize(node.inner_text)
+            # puts ''
+            file.write("* Article #{ix+1}:\n")
+            file.write(ActionView::Base.full_sanitizer.sanitize(node.inner_text))
+            file.write("\n---\n")
+        end
+    end
+    return true
+end
+
+def call_api_for_all_texts
+    Dir.glob("inputs/medium-latest-articles.*.txt") do |my_text_file|
+        puts "Working on: #{my_text_file}..."
+        output_file = "outputs/" + my_text_file.split('/')[1]
+        genai_input = Prompt + "\n" + File.read(my_text_file)
+
+        puts "== INPUT BEGIN: =="
+        puts genai_input
+        #puts :TL_DR
+        puts "== INPUT END =="
+        puts "== OUTPUT BEGIN: =="
+        include LibGenai
+        output = generate(genai_input)
+        File.open(output_file, 'w') do |f|
+            f.write output
+        end
+        puts "== INPUT END =="
+        
+        #exit 42
+        # Call API for summarization: https://cloud.google.com/vertex-ai/docs/generative-ai/text/summarization-prompts
     end
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 def main()
     init()
-    medium_user = ENV.fetch 'MEDIUM_USER_ID', 'palladiusbonton'
+    medium_user = ENV.fetch 'MEDIUM_USER_ID', 'iromin' ##'palladiusbonton'
     fetch_from_medium(medium_user)
+    call_api_for_all_texts()
+    puts('Please check your inputs/ directory for information I gave in input to GenAI and outputs/ for the GenAI Text output.')
 end
 
 main()
