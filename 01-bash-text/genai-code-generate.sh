e3fa3230d4cdc4f7d122020a3f2286d8e60123c7
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-08-30 09:57:06 +0200

    added Text generation

--- 01-bash-text/genai-code-generate.sh
+++ 01-bash-text/genai-code-generate.sh
@@ -13,23 +13,27 @@ function _fatal() {
     exit 42
 }
 function _colorize() {
-    # if you have `lolcat`, even better!
     while read ROW; do 
         echo -en "\033[1;33m$ROW\033[0m\n"
     done
 }
-DEFAULT_MESSAGE="Please write me 3 random genai prompts"
+
+#DEFAULT_MESSAGE="def reverse_string(s):"
+DEFAULT_MESSAGE="def reverse_string(s):
+    return s[::-1]
+def test_empty_input_string()
+"
 
 if [ "$#" -gt 0 ]; then
     export MESSAGE="$*"
 else
-     export MESSAGE="$DEFAULT_MESSAGE"
+    export MESSAGE="$DEFAULT_MESSAGE"
 fi
 
 STRIPPED_MESSAGE=$(echo -en "$MESSAGE" | tr -d '"')
 export PROJECT_ID="$(gcloud config get project)"
 export API_ENDPOINT='us-central1-aiplatform.googleapis.com'
-export MODEL_ID='text-bison@001'
+export MODEL_ID='code-gecko' # @001
 export TOKEN="$(gcloud --project $PROJECT_ID auth print-access-token)"
 
 set -euop pipefail
@@ -44,13 +48,16 @@ if [ 'true' = "$DEBUG" ]; then
     echo "GCLOUD USER: $(gcloud config get account)"
     echo "PROJECT_ID: $PROJECT_ID"
     echo "STRIPPED_MESSAGE: $STRIPPED_MESSAGE"
+    echo 'Code model params: https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/code-chat'
     echo '==================================================================================='
 fi
 
-echo "Input: $STRIPPED_MESSAGE"
-echo Output:
+echo Input: | _colorize 
+echo "$STRIPPED_MESSAGE"
+
 #set -x
 
+# Code readme: https://cloud.google.com/vertex-ai/docs/generative-ai/model-reference/code-chat
 curl -s \
     -X POST \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
@@ -59,24 +66,26 @@ curl -s \
     $'{
     "instances": [
         {
-        "content": "'"$STRIPPED_MESSAGE"'"
+        "prefix": "'"$STRIPPED_MESSAGE"'"
         }
     ],
     "parameters": {
-        "temperature": 0.8,
-        "maxOutputTokens": 1000,
-        "topP": 0.8,
-        "topK": 40
+        "temperature": 0.5,
+        "maxOutputTokens": 64,
+        "candidateCount": 2
     }
-}' > .tmp.lastsong
+}' > .tmp.last_code_output
 
-OUTPUT=$(cat .tmp.lastsong | jq .predictions[0].content)
+OUTPUT=$(cat .tmp.last_code_output | jq .predictions[0].content)
 
 #echo "OUTPUT: '$OUTPUT'"
 if [ "$OUTPUT" = '""' ]; then # empty answer
-    echo "Sorry, some error here. Dig into the JSON file more: .tmp.lastsong" >&2
-    cat .tmp.lastsong | jq >&2
+    echo "Sorry, some error here. Dig into the JSON file more: .tmp.last_code_output" >&2
+    cat .tmp.last_code_output | jq >&2
 else
     #echo Ok non trivial content..
-    cat .tmp.lastsong | jq .predictions[0].content -r  | _colorize # | lolcat
+    echo Output 1: | _colorize
+    cat .tmp.last_code_output | jq .predictions[0].content -r # | _colorize
+    echo Output 2: | _colorize
+    cat .tmp.last_code_output | jq .predictions[1].content -r # | _colorize
 fi
