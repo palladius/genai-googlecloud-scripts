297add0e5a7c6f170e8dc7c1e1c84423a10d44ce
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-10-25 15:22:38 +0200

    removing the double quote trim which seems not needed

--- 06-ruby-text-code-embeddings-images/genai-embeddings-generate.rb
+++ 06-ruby-text-code-embeddings-images/genai-embeddings-generate.rb
@@ -19,9 +19,30 @@ PROJECT = ENV.fetch('PROJECT_ID', `gcloud config get core/project`.chomp)
 OUTPUT_DIR = "out"
 MODEL_ID = "textembedding-gecko" # @001
 MESSAGES = [
-    "Nel mezzo del cammin di nostra vita, mi ritrovai per una selva oscura", # Divine comedy verse 1
-    "Ahi a dir qual era e' cosa dura!", # Divine comedy verse 2
-    "To be or not to  be, *this* is the question!", # Shakespeare somewhat famous sentence
+    "Nel mezzo del cammin di nostra vita
+    mi ritrovai per una selva oscura,
+    ché la diritta via era smarrita.", # Divine comedy verse 1
+
+    "Ahi quanto a dir qual era è cosa dura,
+    esta selva selvaggia e aspra e forte,
+    che nel pensier rinova la paura!", # Divine comedy verse 2
+
+    "Cantami, o Diva, del Pelìde Achille
+    l'ira funesta che infiniti addusse
+    lutti agli Achei, molte anzi tempo all'Orco
+    generose travolse alme d'eroi", # Ilyad, italian proemio
+
+    #testing the double \" quote
+    '"Thou, Nature, art my goddess"', # 7. Edmund in King Lear 1.2, “Thou, Nature, art my goddess”
+
+    # https://en.wikipedia.org/wiki/To_be,_or_not_to_be - divided in 2:
+    # verse 1
+    "To be, or not to be, that is the question:",
+    # verse 2-5    
+    "Whether 'tis nobler in the mind to suffer
+    The slings and arrows of outrageous fortune,
+    Or to take Arms against a Sea of troubles,
+    And by opposing end them: to die, to sleep",
 ]
 
 # assert size is not more than 5 - i think its the limit
@@ -30,17 +51,21 @@ require "base64"
 require "fileutils"
 require "google/apis/aiplatform_v1"
 
+def yellow(s)
+  "\033[1;33m#{s}\033[0m"
+end
 client = Google::Apis::AiplatformV1::AiplatformService.new
 client.root_url = "https://us-central1-aiplatform.googleapis.com/"
 client.authorization = Google::Auth.get_application_default
 
 # Array of hashes: [
-#   {content: "qui"},
-#   {content: "quo"},
-#   {content: "qua"},
+#   {content: "foo"},
+#   {content: "bar"},
+#   {content: "baz"},
 # ]
 
-ArrayOfContentHashes = MESSAGES.map{|message| {content: message.tr('"', "") }}
+#ArrayOfContentHashes = MESSAGES.map{|message| {content: message.tr('"', "") }}
+ArrayOfContentHashes = MESSAGES.map{|message| {content: message }}
 
 request = Google::Apis::AiplatformV1::GoogleCloudAiplatformV1PredictRequest.new \
   instances: ArrayOfContentHashes
@@ -53,11 +78,17 @@ response = client.predict_project_location_publisher_model \
 FileUtils.mkdir_p OUTPUT_DIR
 response.predictions.each_with_index do |prediction, index|
   puts "== Prediction #{index} =="
-  puts "* Original Messages: #{MESSAGES[index]}"
+  puts "* Original Messages: #{yellow MESSAGES[index]}"
   puts "* Statistics: #{prediction['embeddings']['statistics']}"
   puts "* Dimensions: #{prediction['embeddings']['values'].size}"
   filename = "#{OUTPUT_DIR}/embedding-#{index}.txt"
-  File.write filename, prediction["embeddings"]
+  final_hash = {
+    embeddings: prediction["embeddings"],
+    original_message: MESSAGES[index],
+  }
+  #File.write filename, prediction["embeddings"]
+  File.write filename, final_hash # adding both the output that the meaningful input for future reference / crunching.
   puts "* Wrote #{filename}"
   puts ""
 end
+
