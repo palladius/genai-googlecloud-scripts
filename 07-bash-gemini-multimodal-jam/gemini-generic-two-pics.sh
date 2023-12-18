f7ddca7679eb2c08c67cd1e743dd7ca15353d968
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-12-18 16:10:58 +0100

    wow lot of work to use the PROD url!

--- 07-bash-gemini-multimodal-jam/gemini-generic-two-pics.sh
+++ 07-bash-gemini-multimodal-jam/gemini-generic-two-pics.sh
@@ -13,20 +13,11 @@ fi
 #PROJECT_ID='provided in .envrc'
 MODEL_ID="gemini-pro-vision"
 LOCATION=us-central1
-TMP_OUTPUT_FILE=.tmp.lastsaga3
+TMP_OUTPUT_FILE=.tmp.lastresponse-generic-2pix.json
 REQUEST_FILE=.tmp.request-generic-2pix.json
-JQ_PATH=".candidates[0].content.parts[0].text"
-
-#data=$(base64 -i "$IMAGE" -o -) # Mac
-#data=$(base64 -w 0 "$IMAGE") # linux
-function _base64_mac_or_linux() {
-    IMAGE="$1"
-    if [[ $(uname) == "Darwin" ]] ; then
-        base64 -i "$IMAGE" -o -
-    else
-        base64 -w 0 "$IMAGE"
-    fi
-}
+JQ_PATH=".[0].candidates[0].content.parts[0].text" # PROD_URL_SELECTOR
+#STAGING_JQ_PATH=".candidates[0].content.parts[0].text" # STAGING_URL_SELECTOR (changed! Why?)
+JQ_PATH_PLURAL=".[].candidates[0].content.parts[0].text" # PROD_URL_SELECTOR all answers
 
 function _usage() {
     echo "Usage: $0 <IMAGE> <question on that image>"
@@ -49,8 +40,9 @@ fi
 export IMAGE1="$1"
 export IMAGE2="$2"
 
-data1=$(_base64_mac_or_linux "$IMAGE1") # Mac or Linux should both work!
-data2=$(_base64_mac_or_linux "$IMAGE2") # Mac or Linux should both work!
+data1=$(_base64_encode_mac_or_linux "$IMAGE1")
+data2=$(_base64_encode_mac_or_linux "$IMAGE2")
+#shift
 #shift
 #export QUESTION="$@" # should default to "what do you see here?"
 export QUESTION="Can you highlight similarity and differences between the two? Also, do you recognize the same person in both of them?"
@@ -73,25 +65,42 @@ cat > "$REQUEST_FILE" <<EOF
             'data': '$data2',
             'mime_type':'image/jpeg'}
         }
-        ]}
+        ]},
+
+
+  "safety_settings": {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_LOW_AND_ABOVE"
+  },
+  "generation_config": {
+        "temperature": 0.2,
+        "topP": 0.8,
+        "topK": 40,
+        "maxOutputTokens": 1500
+
+  }
 }
 EOF
+#"stopSequences": [".", "?", "!"]
+#STAGING_URL="https://${LOCATION}-autopush-aiplatform.sandbox.googleapis.com/v1beta1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/gemini-pro-vision:generateContent"
+PROD_URL="https://${LOCATION}-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/gemini-pro-vision:streamGenerateContent"
+
 
 curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     -H "Content-Type: application/json"  \
-    https://us-central1-autopush-aiplatform.sandbox.googleapis.com/v1beta1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/gemini-pro-vision:generateContent -d \
+    "$PROD_URL" -d \
     @"$REQUEST_FILE" \
     > $TMP_OUTPUT_FILE 2>t ||
         show_errors_and_exit
 
 #echo "Written $TMP_OUTPUT_FILE. curl_ret=$?"
 
-OUTPUT=$(cat $TMP_OUTPUT_FILE | jq $JQ_PATH)
+OUTPUT=$(cat $TMP_OUTPUT_FILE | jq $JQ_PATH_PLURAL || echo  '""')
 
 if [ "$OUTPUT" = '""' ]; then # empty answer
     echo "‼️ Sorry, some error here. Dig into the JSON file more: $TMP_OUTPUT_FILE" >&2
     cat $TMP_OUTPUT_FILE | jq >&2
 else
     echo '♊️ Describing attached image:'
-    cat $TMP_OUTPUT_FILE | jq "$JQ_PATH" -r | _lolcat
+    cat $TMP_OUTPUT_FILE | jq "$JQ_PATH_PLURAL" -r | _lolcat
 fi
