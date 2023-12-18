4d83f3f5c66445753f67e2bc9e1aeeedaafda10f
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-12-18 17:08:42 +0100

    fixing skye blue too

--- 07-bash-gemini-multimodal-jam/gemini-why-is-the-sky-blue.sh
+++ 07-bash-gemini-multimodal-jam/gemini-why-is-the-sky-blue.sh
@@ -2,20 +2,33 @@
 
 # first: gcloud auth login
 
-PROJECT_ID='cloud-llm-preview1'
+# Verbs:
+# generateContent (used to work in staging, doesnt work in prod)
+# streamGenerateContent (works in PROD)
+
 MODEL_ID="gemini-pro"
-LOCATION=us-central1
+LOCATION="us-central1"
+TMP_OUTPUT_FILE=".tmp.why-sky-blue.json"
+JQ_PATH_PLURAL=".[].candidates[0].content.parts[0].text" # PROD_URL_SELECTOR all answers from StreamGenerateContent
 
 set -euo pipefail
 
 gcloud config set project $PROJECT_ID
 
-echodo curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+# PROD_URL="https://${LOCATION}-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/$MODEL_ID:streamGenerateContent"
+
+curl -X POST \
+    -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     -H "Content-Type: application/json"  \
-    https://us-central1-autopush-aiplatform.sandbox.googleapis.com/v1beta1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/${MODEL_ID}:generateContent -d \
+    https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/publishers/google/models/${MODEL_ID}:streamGenerateContent -d \
     $'{
       "contents": {
         "role": "USER",
         "parts": { "text": "Why is the sky blue?" }
       }
-  }'
+    }'  1>"$TMP_OUTPUT_FILE" 2>/dev/null
+
+CLEANED_OUTPUT="$(cat "$TMP_OUTPUT_FILE" | jq -r "$JQ_PATH_PLURAL" | xargs -0 )"
+
+echo "# ♊ Input: 'Why is the sky blue?'"
+echo "# ♊ Output: '$CLEANED_OUTPUT'"
