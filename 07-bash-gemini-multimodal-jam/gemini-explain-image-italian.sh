4bbbffa99bea07ed2189e723e60ae011e46ea5b7
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-12-18 17:08:42 +0100

    fixing skye blue too

--- 07-bash-gemini-multimodal-jam/gemini-explain-image-italian.sh
+++ 07-bash-gemini-multimodal-jam/gemini-explain-image-italian.sh
@@ -2,5 +2,6 @@
 
 # set -euo pipefail
 
+# alternatively, an even pickier question on 🇮🇹 Italianity of the picture
 #./gemini-generic.sh "$1" "Descrivimi cosa vedi in questa immagine e se ci sono c'e' correlazione alcuna con l'Italia come nazione e come cultura italiana"
-TTS_LANG='it-IT' ./gemini-generic.sh "$1" "Descrivimi cosa vedi in questa immagine"
+TTS_LANG='it-IT' ./gemini-generic.sh "$1" "Descrivimi questa immagine"
