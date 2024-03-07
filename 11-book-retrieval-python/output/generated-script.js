dfa02ab920ae101efe22f09684f5282db5876deb
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-03-07 08:26:43 +0100

    fixed the N/A parts

--- 11-book-retrieval-python/output/generated-script.js
+++ 11-book-retrieval-python/output/generated-script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a26cc94513b8a1136f93d9eca1429aabb87d3164a95eda39eb8c9db1a81eae7
-size 4361
+oid sha256:f757d0efe8cca42530f07369738362b349c681049d93487478a3c108273a8f9d
+size 4608
--- 11-book-retrieval-python/templates/script-after.js
+++ 11-book-retrieval-python/templates/script-after.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57f1738d1a688f539813efb83b39c522475dde3c98746d0dd7fd9b08be3f9d78
-size 1080
+oid sha256:2c3a57cdfdaa2f01ac0e67cca6a942fdb15a92844e44af9ee48e5b82a15ddaa1
+size 1459
