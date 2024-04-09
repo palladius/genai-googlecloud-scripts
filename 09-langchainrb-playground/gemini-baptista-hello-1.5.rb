debf0e7953e43781245a04c7a7f9debcd797453d
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-04-09 05:30:08 -0700

    01 still works great

--- 09-langchainrb-playground/gemini-baptista-hello-1.5.rb
+++ 09-langchainrb-playground/gemini-baptista-hello-1.5.rb
@@ -2,7 +2,7 @@
 
 require 'gemini-ai'
 require_relative 'lib/common'
-require_relative '_env_gaic' # if you use GAIC
+#require_relative '_env_gaic' # if you use GAIC
 
 # python model for Gemini
 # 1.0: model = GenerativeModel("gemini-1.0-pro-001")
@@ -15,9 +15,9 @@ require_relative '_env_gaic' # if you use GAIC
 #GeminiModel = "gemini-2.0-pro-001"
 #GeminiModel = "gemini-1.5" # 404
 #GeminiModel = "gemini-1.5-pro-latest" # 404 https://github.com/google/generative-ai-python/issues/227
-GeminiModel = "gemini-1.5-pro-eval" # 404
+#GeminiModel = "gemini-1.5-pro-eval" # 404
 #GeminiModel = "gemini-1.5-pro" # 404
-#GeminiModel = "gemini-1.5-pro-preview-0215" # 400 doesnt work
+GeminiModel = "gemini-1.5-pro-preview-0215" # 400 doesnt work
 
 print("\n\n ♊️ Using GeminiModel=#{GeminiModel}\n ♊️ auto_project_id(): #{auto_project_id}\n\n")
 
@@ -34,15 +34,19 @@ client = Gemini.new(
     server_sent_events: true }
 )
 
-# result = client.stream_generate_content({
-#   contents: { role: 'user', parts: { text: 'hi!' } }
-# })
-# present_gemini_result(result, debug: true)
-
 result = client.stream_generate_content({
-  contents: { role: 'user',
-    parts: {
-      text: 'Can you write a little salutation to me like you were impersonating Marvin the Paranoid Android from the HitchHikers guide to the galazy from Douglas Adams? I would expect this to be pessimistic, vaguely rude and very humorous!'
-      } }
+  contents: {
+    role: 'user',
+    parts: { text: 'hi!' } }
 })
+present_gemini_result(result, debug: true)
+
+# result = client.stream_generate_content({
+#   contents: {
+#     role: 'user',
+#     parts: {
+#       text: 'Can you write a little salutation to me like you were impersonating Marvin the Paranoid Android from the HitchHikers guide to the galazy from Douglas Adams? I would expect this to be pessimistic, vaguely rude and very humorous!'
+#       } }
+# })
+print result
 present_gemini_result(result, debug: false)
