4cef86c23606e96b64a794c2299c9afb3d8251e1
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-10 10:39:22 +0200

    rubcopped

--- 15-ollama-gemma-RAG-curl-ruby/wasser-temperatur-rag.rb
+++ 15-ollama-gemma-RAG-curl-ruby/wasser-temperatur-rag.rb
@@ -1,40 +1,37 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'erb'
 require 'date'
 require_relative './curlit'
 require 'open3' # For Open3.pipeline
 
-ScriptVersion = '0.2'
+SCRIPT_VERSION = '0.2'
 
 # wrap included library
 def curlit(url:)
-  extract_text_from_url(url:)
+  extract_text_from_url url: url
 end
-
 
 # Takes the prompt, and substitutes with some variables
 def substitute_with_rag(template_path:, rag_content:)
   # 1. Load the ERB Template
-
   template_content = File.read(template_path)
 
   erb_template = ERB.new(template_content)
 
   sample_variables = {
     date_today: Date.today,
-    rag_from_website: rag_content,
+    rag_from_website: rag_content
   }
-  binding_object = binding
   sample_variables.each do |key, value|
-      binding_object.local_variable_set(key, value)
+    binding.local_variable_set(key, value)
   end
 
   # 5. Render the Template
-  rendered_output = erb_template.result(binding_object)
+  erb_template.result(binding_object)
 
-  # 6. Display/return the Result
-  rendered_output
+  # 6. return the Result
 end
 
 # Uses `popen()` method - after trying and failing with others.
@@ -49,7 +46,7 @@ def pipe_into_gemma(rendered_output:)
   end
 end
 
-# TODO sanitization will go here
+# TODO: sanitization will go here
 def save_sanitized_output_as_json(json_output:, file_name:)
   # TODO(ricc): if output is something like:
   # {
@@ -67,15 +64,13 @@ def main(verbose: true)
   # 2. renders the prompt with this information
   puts("RAG Content: '''#{rag_content}'''") if verbose
 
-  rendered_output = substitute_with_rag(template_path: 'wasser-temperatur-rag.prompt.erb', rag_content:)
+  rendered_output = substitute_with_rag(template_path: 'wasser-temperatur-rag.prompt.erb', rag_content: rag_content)
   # 3. Calling Ollama takes a while, better to tell the user they'll have to wait a bit
-  puts "⌛ Now lets wait for gemma to process it.. popen" if verbose
+  puts '⌛ Now lets wait for gemma to process it..' if verbose
   # 4. why piping in bash when you can do it natively in ruby and manage errors here?
-  json_output = pipe_into_gemma(rendered_output:)
+  json_output = pipe_into_gemma rendered_output: rendered_output
   # 5. Save onto JSON -> see
-  save_sanitized_output_as_json(json_output:, file_name: 'output.json')
+  save_sanitized_output_as_json(json_output: json_output, file_name: 'output.json')
 end
 
-if $0 ==  __FILE__
-  main
-end
+main if $PROGRAM_NAME == __FILE__
