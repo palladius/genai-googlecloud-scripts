0aea9836bde42384cb423ee486faaedb44a5be0b
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-10 07:08:58 +0200

    going to work now. Have to commit all

--- 13-ruby-nanobots/test1.rb
+++ 13-ruby-nanobots/test1.rb
@@ -2,7 +2,7 @@
 
 require 'nano-bots'
 
-bot = NanoBot.new(cartridge: 'gpt.yml')
+bot = NanoBot.new(cartridge: 'cartridges/gpt.yml')
 
 bot.eval('Hi!') do |_content, fragment, _finished, _meta|
   print fragment unless fragment.nil?
