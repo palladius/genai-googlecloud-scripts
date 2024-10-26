a7ce4dc70ce43d9e0106bb1b1dd5caccb5a01065
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-10-26 17:33:57 +0200

    some old stuff form my Mac

---
+++ 12-gemini-embeddings/gemini-baptista-embeddings-test.rb
@@ -0,0 +1,58 @@
+#! /usr/bin/env ruby
+
+require 'gemini-ai'
+require_relative 'lib/common'
+require_relative '_env_gaic' # if you use GAIC
+require 'colorize'
+
+# python model for Gemini
+# 1.0: model = GenerativeModel("gemini-1.0-pro-001")
+
+# THIS WORKS! 'gemini-pro'
+GeminiModel = "gemini-pro" # This ALSO works, yay!
+GeminiModel = "gemini-1.0-pro" # This ALSO works, yay! https://ai.google.dev/models/gemini
+#GeminiModel = "gemini-1.0-pro-001" # This ALSO works, yay!
+#GeminiModel = "gemini-1.0-ultra-001"
+#GeminiModel = "gemini-1.0-ultra"
+# Trying out... nope they wont work
+#GeminiModel = "gemini-1.5-pro-001" # doesnt work on PBT
+#GeminiModel = "gemini-1.5" # doesnt work on PBT
+#GeminiModel = "gemini-2.0-pro-001"
+#GeminiModel = "gemini-1.5"
+#GeminiModel = "gemini-1.5-pro-preview-0215"
+print("\n\n ♊️ Using GeminiModel=#{GeminiModel}\n auto_project_id(): #{auto_project_id}\n")
+
+# With Application Default Credentials
+client = Gemini.new(
+  credentials: {
+    service: 'vertex-ai-api',
+    region: 'us-central1',
+    project_id: auto_project_id()
+  },
+  options: {
+    model: GeminiModel,
+    server_sent_events: true }
+)
+
+# result = client.stream_generate_content({
+#   contents: { role: 'user', parts: { text: 'hi!' } }
+# })
+# present_gemini_result(result, debug: true)
+
+def pure_gemini_llm_predict(my_text: 'Hi')
+  puts("Calling gemini on this text: ''#{my_text}''")
+  result = client.stream_generate_content({
+    contents: { role: 'user',
+      parts: {
+        text: 'Can you write a little salutation to me like you were impersonating Marvin the Paranoid Android from the HitchHikers guide to the galazy from Douglas Adams? I would expect this to be pessimistic, vaguely rude and very humorous!'
+        } }
+  })
+  present_gemini_result(result, debug: false)
+end
+
+
+
+def main
+  #pure_gemini_llm_predict my_text: 'Can you write a little salutation to me like you were impersonating Marvin the Paranoid Android from the HitchHikers guide to the galazy from Douglas Adams? I would expect this to be pessimistic, vaguely rude and very humorous!'
+  pure_gemini_llm_predict my_text: 'Whats the radius of planet Earth?'
+end
