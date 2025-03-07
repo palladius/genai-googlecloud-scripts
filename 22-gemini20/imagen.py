fd7df3fced15b4a3429d4f29efed23911cdc1564
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-03-07 10:52:57 +0100

    imagen works AMAZINGLY

--- 22-gemini20/imagen.py
+++ 22-gemini20/imagen.py
@@ -12,7 +12,7 @@ from colorama import Fore, Style
 
 load_dotenv()
 
-APP_VERSION = '1.0'
+APP_VERSION = '1.1'
 APP_NAME = 'Image Generation Tool'
 
 #IMAGE_PROMPT = 'Fuzzy bunnies in my kitchen, eating a carrot and dipping their paws onto a fondue.'
@@ -47,12 +47,41 @@ def create_filename_from_prompt(prompt, extension='png'):
     return ...
 
 
-def create_filename_from_prompt(prompt, id='', extension='png'):
+# def create_filename_from_prompt_old(prompt, id='', extension='png'):
+#     '''Generate a possible filename based on a generic genai prompt.
+
+#     - if prompt is long, it should be shortened/chopped to max 64 chars.
+#     - all spaces should be moved to underscores.
+#     - a YYYYMMDD date should be prepended for alpha sorting.
+
+#     Should have some sort of ID.
+#     '''
+#     max_length = 96 # 64
+
+#     # Get current date in YYYYMMDD format
+#     today = datetime.date.today().strftime("%Y%m%d")
+
+#     # Shorten the prompt if it's too long
+#     if len(prompt) > max_length:
+#         prompt = prompt[:max_length]
+
+#     # Replace spaces with underscores
+#     prompt = prompt.replace(" ", "_")
+
+#     # Remove any characters that are not alphanumeric or underscores
+#     prompt = re.sub(r'[^a-zA-Z0-9_]', '', prompt)
+
+#     # Construct the filename
+#     filename = f"{today}_{prompt}_{id}.{extension}"
+#     return filename
+
+def create_filename_from_prompt(prompt, id='', extension='png', out_folder='out/'):
     '''Generate a possible filename based on a generic genai prompt.
 
     - if prompt is long, it should be shortened/chopped to max 64 chars.
     - all spaces should be moved to underscores.
     - a YYYYMMDD date should be prepended for alpha sorting.
+    - if out_folder is not None, it will be prepended to the filename.
 
     Should have some sort of ID.
     '''
@@ -73,8 +102,13 @@ def create_filename_from_prompt(prompt, id='', extension='png'):
 
     # Construct the filename
     filename = f"{today}_{prompt}_{id}.{extension}"
-    return filename
 
+    if out_folder is not None:
+        # Ensure the output folder exists
+        os.makedirs(out_folder, exist_ok=True)
+        filename = os.path.join(out_folder, filename)
+
+    return filename
 
 def main():
     if len(sys.argv) < 2 or "--help" in sys.argv:
