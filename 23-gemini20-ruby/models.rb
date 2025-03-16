2b5d0a9b25f8ccf3702d57d75b72f0cb05f5c30a
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-03-16 16:17:15 +0100

    image generation works.

--- 23-gemini20-ruby/models.rb
+++ 23-gemini20-ruby/models.rb
@@ -6,9 +6,11 @@ puts "Total models: #{all_models.count}"
 #puts(all_models.first.inspect)
 google_models = all_models.select{|m| m.provider == 'gemini'}
 puts "Total Google models: #{google_models.count}"
-google_models.each do |model|
-  puts("🧠 #{model.id}" )
-end
+# google_models.each do |model|
+#   puts("MODEL 🧠 #{model.id}" )
+# end
 
+google_vision_models = RubyLLM.models.by_provider('gemini').chat_models.select(&:supports_vision)
+puts("👀 Gemini vision models: #{google_vision_models.map{|m| m.id}.join ', '}")
 
 #puts(response.content)
--- 23-gemini20-ruby/streaming.rb
+++ 23-gemini20-ruby/streaming.rb
@@ -1,13 +1,8 @@
-# require 'ruby_llm'
 require 'ruby_llm'
 require 'rainbow'
 
 require_relative 'lib/common'
 
-# help me write a function which puts() strings n the four different google logo colors, rotating: red blue green yellow and back again. It will probably use a global var to keep state
-def print_rotating_color(message)
-
-end
 
 # RubyLLM.configure do |config|
 #   config.gemini_api_key = ENV['GEMINI_API_KEY']
@@ -17,7 +12,8 @@ end
 
 final_message = GeminiChat.ask "Write a short story about a programmer" do |chunk|
   # Each chunk contains a portion of the response
-  print "[#{chunk.input_tokens}/#{chunk.output_tokens}] #{chunk.content}"
+#  print "[#{chunk.input_tokens}/#{chunk.output_tokens}] #{chunk.content}"
+  print_rotating_color_no_newline chunk.content
 end
 
 puts "\nFinal message length: #{final_message.content.length}"
