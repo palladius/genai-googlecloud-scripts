04626401f42736a0ad21e102d7c23109137a1924
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-04-07 12:44:10 +0200

    code for game 02

---
+++ 24-gemini25-games/02-smurfs-and-dragons/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58a00a46575a000ecb5b4ea3ca06621460dc084e9068af827acea0d28fc65b7d
+size 5163
---
+++ 24-gemini25-games/02-smurfs-and-dragons/public/sketch.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6c118fb6dd0274f0941a8b931d0085a5b6d047c1c73e2b3e54d7e4240fb44d1
+size 27197
