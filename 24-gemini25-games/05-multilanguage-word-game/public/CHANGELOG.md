a49197735b8c655ba8e74a9cc71ae2ded56d90af
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-04-07 12:04:54 +0200

    still broken

--- 24-gemini25-games/05-multilanguage-word-game/public/CHANGELOG.md
+++ 24-gemini25-games/05-multilanguage-word-game/public/CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8caf2fb9b4f0122557d03e6a45ceabdd86e4b2caa5361633301465129682caa
-size 618
+oid sha256:60b18b2c15416f1227bf1575c66955ff4ae3d103ebc986979001d8d97d1eb260
+size 734
--- 24-gemini25-games/05-multilanguage-word-game/public/sketch.js
+++ 24-gemini25-games/05-multilanguage-word-game/public/sketch.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3833128ca4b81b48c5182efcba65d215d661437ca744ecaa806593e5f4475141
-size 29532
+oid sha256:9d198ba2381c8637cbe0a677df681c1ef0dea8c77cb2a990559aa008a579d921
+size 31717
