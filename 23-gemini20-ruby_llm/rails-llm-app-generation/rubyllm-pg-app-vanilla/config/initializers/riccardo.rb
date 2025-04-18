ec3569775b7e71fb48b3a570c79ee739597f0759
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-04-18 19:08:59 +0200

    0.2 cleaned up chat interface

--- 23-gemini20-ruby_llm/rails-llm-app-generation/rubyllm-pg-app-vanilla/config/initializers/riccardo.rb
+++ 23-gemini20-ruby_llm/rails-llm-app-generation/rubyllm-pg-app-vanilla/config/initializers/riccardo.rb
@@ -3,4 +3,4 @@
 
 APP_VERSION = File.read('VERSION').chomp
 APP_NAME = 'LLMRails App'
-APP_DESCRIPION = 'A proof of concept Rails App leveraging RubyLLM and Gemini'
+APP_DESCRIPTION = 'A proof of concept Rails App leveraging RubyLLM and Gemini'
