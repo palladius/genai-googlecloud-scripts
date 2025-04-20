0508b0766b3f66c31a195cf4cb24b15eef493140
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-04-20 07:11:11 +0200

    first version which kind of works

---
+++ 24-gemini25-games/07-pacman-clone/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2467f02660a791d33a72d29c00e6230d229dee7dacbccce64b5ea643e53b88ad
+size 751
---
+++ 24-gemini25-games/07-pacman-clone/public/sketch.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:069cf5a697cc48ab2f086d92d597cf127d941924c5e8dbac97914de3f6ac9759
+size 38706
