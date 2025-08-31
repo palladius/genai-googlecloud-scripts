24417e89dc705636df18b9298fb89e599dba6d65
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-08-31 21:43:20 +0200

    my fgirst golang hello script works!

---
+++ 26-nanobanana-golang/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7a87c2d00668059587aa301330d3df2abf25d0ff16a698e51d42cc26bb0ad8b
+size 47
---
+++ 26-nanobanana-golang/main.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7110e141fdaf719636449c655352041bfea2e965577205d4f578887d2c1b6804
+size 823
