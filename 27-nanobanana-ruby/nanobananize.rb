650aa1155862ab173a0e50777f5e95c029003f36
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-09-01 21:35:32 +0200

    Made with Ruby woohoo

--- 27-nanobanana-ruby/nanobananize.rb
+++ 27-nanobanana-ruby/nanobananize.rb
@@ -9,7 +9,7 @@ require 'dotenv'
 Dotenv.load
 
 if ARGV.length != 2
-  puts "Usage: #{$PROGRAM_NAME} <image_file> <prompt>"
+  puts "Usage: #{ $PROGRAM_NAME } <image_file> <prompt>"
   exit 1
 end
 
@@ -42,7 +42,7 @@ request_body = {
   }
 }.to_json
 
-uri = URI.parse("https://generativelanguage.googleapis.com/v1beta/models/#{model_id}:#{generate_content_api}?key=#{gemini_api_key}")
+uri = URI.parse("https://generativelanguage.googleapis.com/v1beta/models/#{ model_id }:#{ generate_content_api }?key=#{ gemini_api_key }")
 
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -52,16 +52,26 @@ request.body = request_body
 
 response = http.request(request)
 
+
+
+# Define output filenames
+base_dir = File.dirname(image_path)
+base_name = File.basename(image_path, '.*')
+prompt_suffix = prompt.gsub(/[^a-zA-Z0-9_\-+]/, '_').gsub(/\s+/, '_').downcase
+output_json_path = File.join(base_dir, "#{ base_name }.#{ prompt_suffix }.json")
+output_image_path = File.join(base_dir, "#{ base_name }.#{ prompt_suffix }.png")
+
+
 # Write the JSON response to a file
-File.write('nanobanana.response.json', response.body)
-puts "📝 JSON written to nanobanana.response.json"
+File.write(output_json_path, response.body)
+puts "📝 JSON written to #{ output_json_path }"
 
 # Parse the JSON response
 response_data = JSON.parse(response.body)
 
 # Check for errors
 if response_data.first.key?('error')
-  puts "\n🛑 API Error: #{response_data.first['error']['message']}"
+  puts "\n🛑 API Error: #{ response_data.first['error']['message'] }"
   exit 1
 end
 
@@ -74,9 +84,9 @@ else
   b64_data = response_data.last['candidates'].first['content']['parts'].first['inlineData']['data']
 
   # Decode the base64 data and write it to a file
-  File.open('nanobanana.response.png', 'wb') do |f|
+  File.open(output_image_path, 'wb') do |f|
     f.write(Base64.strict_decode64(b64_data))
   end
 
-  puts "🖼️ Image extracted to nanobanana.response.png"
+  puts "🖼️ Image extracted to #{ output_image_path }"
 end
